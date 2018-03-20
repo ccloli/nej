de4519e787d4ab8d997cdc3ff1d2ebad7bf9e6cc
ccloli <h@ccloli.com>
2018-03-20 19:05:03 +0800

    fix src/ui/timepick/test
    it should be 'timepick', not 'datepick'

--- src/ui/timepick/test/timepick.test.js
+++ src/ui/timepick/test/timepick.test.js
@@ -1,6 +1,6 @@
 var f = function(){
     //定义测试模块
-    module("datepick");
+    module("timepick");
     var _  = NEJ.P,
         _e = _('nej.e'),
         _p = _('nej.ui'),
@@ -8,10 +8,10 @@ var f = function(){
 
 
     //开始单元测试
-    test('datepick-ui',function(){
+    test('timepick-ui',function(){
         stop();
-        var _dp = _p._$$DatePick._$allocate({
-            parent:_e._$get('datepick-box'),
+        var _dp = _p._$$TimePick._$allocate({
+            parent:_e._$get('timepick-box'),
             onchange:function(_date){
                 ok(true,'选择了一个日期，返回此日期'+_date);
                 start();
@@ -21,7 +21,7 @@ var f = function(){
 		_dp._$getDate();
     });
 
-    test('datepick-util',function(){
+    test('timepick-util',function(){
         expect(0);
         var pDate = new Date(1997,7,9)
         var nDate = new Date(2013,7,9);
@@ -29,7 +29,7 @@ var f = function(){
         _html = _e._$add('{list 1..2 as x}<div>{list 1..6 as y}<p class="z-day"></p>{/list}</div>{/list}');
         _days.innerHTML = _e._$get(_html);
         var _dp = _ut._$$Calendar._$allocate({
-            parent:_e._$get('datepick-box2'),
+            parent:_e._$get('timepick-box2'),
             offset:1,
             list:_e._$getByClassName(_days,"z-day"),
             year:_e._$get('year'),
@@ -55,5 +55,5 @@ var f = function(){
 }
 module('依赖模块');
 test('define',function(){expect(0);
-    define('{pro}datepick.test.js',['ui/datepick/datepick'],f);
+    define('{pro}timepick.test.js',['ui/timepick/timepick'],f);
 });
